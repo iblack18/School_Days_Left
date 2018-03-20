ee1b052a2d1e6fb3569894abed05355ccf1e1abf
Gus Beringer <gusberinger@gmail.com>
2018-03-19 20:33:54 -0500

    first commit

---
+++ days_left.py
@@ -0,0 +1,39 @@
+import datetime
+
+def school_day(date):
+    if date.weekday() == 6 and date.weekday() == 5:
+        return False
+    else:
+        return True
+
+def holiday(date):
+    if date.month == 3:
+        spring_break = range(26, 31)
+        if date.day in spring_break:
+            return True
+        else:
+            return False
+    elif date.month == 4:
+        # Easter Monday
+        if date.day == 2:
+            return True
+        else:
+            return False
+    else:
+        return False
+
+def school_end(date):
+    if date.day == 23:
+        if date.month == 5:
+            return True
+
+def days(date):
+    days = 0
+    while True:
+        date += datetime.timedelta(days=1)
+        if school_day(date) == True:
+            if holiday(date) == False:
+                days += 1
+        if school_end(date) == True:
+            break
+    return days
